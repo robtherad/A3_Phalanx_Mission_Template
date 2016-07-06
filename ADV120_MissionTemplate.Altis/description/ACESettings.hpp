ce6e788a59f9965655074c186e0195a0e5d2eda1
robtherad <robtherad@gmail.com>
2016-07-06 09:27:19 -0400

    Include ACE settings in mission

---
+++ ADV120_MissionTemplate.Altis/description/ACESettings.hpp
@@ -0,0 +1,27 @@
+class ACE_Settings {
+    class ace_finger_enabled {
+        value = 1;
+        typeName = "BOOL";
+        force = 0;
+    };
+    class ace_frag_reflectionsEnabled {
+        value = 1;
+        typeName = "BOOL";
+        force = 0;
+    };
+    class ace_map_mapIllumination {
+        value = 0;
+        typeName = "BOOL";
+        force = 0;
+    };
+    class ace_map_mapGlow {
+        value = 0;
+        typeName = "BOOL";
+        force = 0;
+    };
+    class ace_map_mapShake {
+        value = 0;
+        typeName = "BOOL";
+        force = 0;
+    };
+};
